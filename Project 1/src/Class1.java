7074e9d6fc0fa8b33e7df083845c53f2ecab52ca
mhs <mhs@MHS-Science2-20>
2017-10-11 17:00:17 -0700

    Loops

---
+++ Project 1/src/Class1.java
@@ -0,0 +1,29 @@
+
+public class Class1 {
+	
+	public static void main(String[] args) {
+		int x=5;
+		int y=9;
+		int z=16;
+		
+		if (x>y && x>z) {
+			System.out.println(x);
+		}
+		if (y>x && y>z) {
+			System.out.println(y);
+		}
+		if (z>x && z>y) {
+			System.out.println(z);
+		}
+	
+		int counter =0;
+		while(counter<3){
+			System.out.println("go programming");
+			counter++;
+		}
+		
+		int i;
+		for(i=0; i<2; i++) {
+			System.out.println(i);
+		}
+}}
